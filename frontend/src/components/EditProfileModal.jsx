d5abc03061f42e3cf4a8f6523244e1f056e686be
DilekaTharuki <dilekagunawardana21@gmail.com>
2025-05-05 20:29:22 +0530

    Fix some issues in frontend

--- frontend/src/components/EditProfileModal.jsx
+++ frontend/src/components/EditProfileModal.jsx
@@ -13,7 +13,7 @@ export default function EditProfileModal({
     successMessage
 }) {
   return (
-    <div>EditProfileModal
+    <div>
         {isEditModalOpen && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
             <div className="bg-white rounded-lg p-6 w-full max-w-md">
