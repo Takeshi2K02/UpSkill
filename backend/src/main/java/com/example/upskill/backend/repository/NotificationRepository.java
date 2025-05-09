47b04e0f2762b25ee18d9749ed8d40905f8e7014
Takeshi Dilshan <115790936+Takeshi2K02@users.noreply.github.com>
2025-05-09 21:42:39 +0530

    notification implementation

--- backend/src/main/java/com/example/upskill/backend/repository/NotificationRepository.java
+++ backend/src/main/java/com/example/upskill/backend/repository/NotificationRepository.java
@@ -1,12 +1,10 @@
 package com.example.upskill.backend.repository;
 
-import java.util.List;
-
+import com.example.upskill.backend.model.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.example.upskill.backend.model.Notification;
+import java.util.List;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
-    List<Notification> findByUserIdAndIsRead(String userId, boolean isRead);
-    List<Notification> findByUserId(String userId);
+    List<Notification> findByRecipientIdOrderByCreatedAtDesc(String recipientId);
 }
